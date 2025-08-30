2955b462fdefc1c316c1e55fd4f8f3d6531f5cde
rhunter654 <rhunter654@protonmail.com>
2025-08-30 05:58:22 -0400

    Update ble_wifi_scanner_config_Version2.py

--- src/ble_wifi_scanner_config_Version2.py
+++ src/ble_wifi_scanner_config_Version2.py
@@ -1,3 +1,14 @@
-# Placeholder content for ble_wifi_scanner_config_Version2.py
+"""
+Central configuration for BLE/Wi-Fi scanner.
+Edit constants here for easy global changes.
+"""
 
-# TODO: Implement the actual functionality
+# Example config variables
+WIFI_IFACE = "wlp5s0"
+WIFI_SCAN_INTERVAL_S = 5.0
+BLE_SCAN_INTERVAL_S = 5.0
+BLE_SCAN_DURATION_S = 4.5
+PLOT_REFRESH_INTERVAL_S = 1.0
+MAX_HISTORY_ROWS = 2000
+LOG_DIR = "./logs"
+# ... Add all other config constants here
